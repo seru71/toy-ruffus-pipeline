2a44adf875f5af48ec033c707e9c9d32641b2eab
dsielski <dawid.sielski@outlook.com>
2021-02-09 10:42:20 +0100

    added tests

--- tests/test_mnm_drmaa_wrapper.py
+++ tests/test_mnm_drmaa_wrapper.py
@@ -1,9 +1,21 @@
 import unittest
+from unittest.mock import Mock
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class DrmaaWrapperTest(unittest.TestCase):
+
+    mock_session = Mock
+    job_scrip_directory = "/tmp"
+
+
+    def test_run_job(self):
+        from mnm_drmaa_wrapper import run_job
+
+        run_job("echo Hello",
+                job_script_directory='/tmp/drmaa',
+                run_locally = False,
+                session = self.mock_session)
+
 
 
 if __name__ == '__main__':
